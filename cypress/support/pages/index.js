bce5934c3857e805c4e663505452be56f584a7aa
vzp777 <victorzamanapenna@gmail.com>
2024-02-20 20:31:59 -0300

    Exercicio Modulo 22

---
+++ cypress/support/pages/index.js
@@ -0,0 +1,6 @@
+import { dashboardPage } from './dashboard.page'
+import RegisterPage from './register.page'
+import { accountPage } from './account.page'
+import CheckoutPage from './checkout.page'
+
+export { dashboardPage, RegisterPage, accountPage, CheckoutPage}
